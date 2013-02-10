2cc2303fc76a5c3c8c7c5cadda076b7d04c1e2a1
Sam <samuel@pagedegeek.com>
2013-02-10 21:30:13 +0100

    add test

--- spec/tonality_analyser_spec.rb
+++ spec/tonality_analyser_spec.rb
@@ -12,4 +12,16 @@ describe TonalityAnalyser::Engine do
     e.tonality('want') == :pos
     e.tonality('Thanks') == :pos
   end
+
+  it 'propose tonality' do
+    e = TonalityAnalyser::Engine.new
+    e.train "c'est super !", :pos
+    e.train "encore un beau ", :pos
+    e.train "nul nul nul", :neg
+    e.train "pas tres beau projet", :neg
+    e.compute_probabilities!
+    e.tonality('super beau projet').should == :pos
+    e.tonality('c\'est nul').should == :neg
+  end
+
 end
